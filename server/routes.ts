52cd64dff22274720ac0bf39ef99f6d761139a6b
Samuel Gregory <samuelgregory@me.com>
2025-11-18 13:25:05 +0000

    fix: remove section-metadata route import after file deletion

--- server/routes.ts
+++ server/routes.ts
@@ -14,7 +14,6 @@ import { registerHiddenSectionsRoutes } from "./routes/hidden-sections";
 import { registerInspirationBoardsRoutes } from "./routes/inspiration-boards";
 import { registerInvitationRoutes } from "./routes/invitations";
 import { registerPersonasRoutes } from "./routes/personas";
-import { registerSectionMetadataRoutes } from "./routes/section-metadata";
 import { registerSlackRoutes } from "./routes/slack";
 import { registerSlackOAuthRoutes } from "./routes/slack-oauth";
 import { registerTypeScalesRoutes } from "./routes/type-scales";
@@ -100,7 +99,6 @@ export function registerRoutes(app: Express) {
   registerPersonasRoutes(app);
   registerInspirationBoardsRoutes(app);
   registerHiddenSectionsRoutes(app);
-  registerSectionMetadataRoutes(app);
   registerTypeScalesRoutes(app);
   registerFigmaRoutes(app);
   registerGoogleDriveRoutes(app);
